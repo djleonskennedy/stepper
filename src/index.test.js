a890107cebac9acaa4e067b56dc02d2832b9bbc4
Yuriy Yakovenko <yuriy.yakovenko@infopulse.com>
2016-09-13 16:43:56 +0300

    add commitizen to project

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var EasyStep = require('../dist/index');
+
+describe('easy-step', function() {
+  it('should work!', function () {
+    expect(true).to.be.true;
+  })
+});
